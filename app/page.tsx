ff8a3e5750585a40a05891b22c872b2cba61ac55
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-07-31 18:27:43 +0100

    build; Added cell component, imported and styled it in game board

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
+import Cell from "./components/cell";
 
 export default function Home() {
+  // Import the Cell component
+  const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
+
+  // Render the gameboard with cells
   return (
       <div className="container" >
         <div className="gameboard">
-          
+          {cells.map((cell, index) => (
+            <Cell key={index} />
+          ))}
         </div>
       </div>
   );
