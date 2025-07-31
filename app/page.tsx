07cdb8ebf6b58bca82a62c9b82c68d4fbb13a764
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-07-31 18:56:07 +0100

    feat; added parameters used to identify cells in component file.

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,14 @@ import Cell from "./components/cell";
 export default function Home() {
   // Import the Cell component
   const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [go, setGo] = useState("circle");
 
   // Render the gameboard with cells
   return (
       <div className="container" >
         <div className="gameboard">
           {cells.map((cell, index) => (
-            <Cell key={index} />
+            <Cell id={index} go={go} setGo={setGo} key={index} cells={cells} setCells={setCells} />
           ))}
         </div>
       </div>
