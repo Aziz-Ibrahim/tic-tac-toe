4b604fb5f179e41f71d82986f193c7f37dba565d
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-08-02 16:07:23 +0100

    feat; Added logic and styles to display turn played and next turn

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,48 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Cell from "./components/cell";
+
+const WiningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
 
 export default function Home() {
   // Import the Cell component
   const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
   const [go, setGo] = useState("circle");
 
+  useEffect(() => {
+    // Check for a winner after each move
+    WiningCombinations.forEach(combination => {})
+  }, [cells]);
+
   // Render the gameboard with cells
   return (
       <div className="container" >
         <div className="gameboard">
           {cells.map((cell, index) => (
-            <Cell id={index} go={go} setGo={setGo} key={index} cells={cells} setCells={setCells} />
+            <Cell
+              id={index}
+              go={go} 
+              setGo={setGo}
+              key={index} 
+              cells={cells} 
+              setCells={setCells} 
+              cell={cell}
+            />
           ))}
+        </div>
+        <div className="status">
+          <h2>Next turn: {go === "circle" ? "O" : "X"}</h2>
         </div>
       </div>
   );
